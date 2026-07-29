c0a1a2b169c27efd096e02cbea66f7bf099ccc6d
Vaibhav <vaibhav.saravanan@gmail.com>
2026-07-28 22:09:16 -0400

    style: add colors and text size

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div class="Splash-Page Intro">
-        <h3 className="text-l">Hi I'm</h3>
-        <h1 className="text-3xl">Vaibhav Saravanan</h1>
-        <h3>Student Developer</h3>
-        <p>I'm passionate about building software that helps those around the world as well as learning new things</p>
-        <button>Learn More</button>
+      <div className="Splash-Page Intro">
+        <h3 className="text-lg text-accent font-bold">Hi, I'm</h3>
+        <h1 className="text-5xl text-primary font-extrabold">Vaibhav Saravanan</h1>
+        <h3 className="text-lg text-accent font-bold">Student Developer</h3>
+        <p className="text-sm text-secondary text">
+          I'm passionate about building software that helps those around the
+          world as well as learning new things.
+        </p>
+        <button className="bg-accent rounded-xl">Learn More</button>
       </div>
-      
-      <div class="Splash-Page Interests">
-        <h1>Interests</h1>
-        <div class="Splash-Page-Cards">
-          <div class="Splash-Page-Card">
-            <p>Web Development</p>
+
+      <div className="Splash-Page Interests">
+        <h1 className="text-2xl font-bold text-primary">Interests</h1>
+
+        <div className="Splash-Page-Cards">
+          <div className="Splash-Page-Card">
+            <p className="text-card">Image</p>
+            <p className="text-primary">Web Development</p>
+            <p className="text-secondary">Description</p>
           </div>
-          <div class="Splash-Page-Card">
-            <p>App Development</p>
+
+          <div className="Splash-Page-Card">
+            <p className="text-card">Image</p>
+            <p className="text-primary">App Development</p>
+            <p className="text-secondary">Description</p>
           </div>
-          <div class="Splash-Page-Card">
-            <p>Problem Solving</p>
+
+          <div className="Splash-Page-Card">
+            <p className="text-card">Image</p>
+            <p className="text-primary">Problem Solving</p>
+            <p className="text-secondary">Description</p>
           </div>
-          <div class="Splash-Page-Card">
-            <p>Cooking</p>
+
+          <div className="Splash-Page-Card">
+            <p className="text-card">Image</p>
+            <p className="text-primary">Cooking</p>
+            <p className="text-secondary">Description</p>
           </div>
-          <div class="Splash-Page-Card">
-            <p>Content Creation</p>
+
+          <div className="Splash-Page-Card">
+            <p className="text-card">Image</p>
+            <p className="text-primary">Content Creation</p>
+            <p className="text-secondary">Description</p>
           </div>
         </div>
       </div>
 
-      <div class="Splash-Page Future-Projects">
-        <h1>My Projects</h1>
-        <p>Feel free to check out some of my cool projects</p>
-        <button>View Projects</button>
+      <div className="Splash-Page Future-Projects">
+        <h1 className="text-2xl font-bold text-primary">My Projects</h1>
+        <p className="text-secondary">Feel free to check out some of my cool projects!.</p>
+        <button className="text-accent">View Projects</button>
       </div>
     </>
   )
