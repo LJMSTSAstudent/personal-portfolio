25408063ca9eff0220f9d5435835ea12245d19bc
Vaibhav <vaibhav.saravanan@gmail.com>
2026-07-30 15:35:23 -0400

    style: add interest card icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 
-import { FaGithub, FaLinkedin, FaArrowRight, FaLaptopCode, FaEnvelope } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
+import { FaGithub, FaLinkedin, FaArrowRight, FaLaptopCode, FaEnvelope, FaChartPie } from "react-icons/fa";
+import { MdEmail, MdOutlineComputer } from "react-icons/md";
+import { IoCamera } from "react-icons/io5";
+import { TbChefHatFilled } from "react-icons/tb";
+import { IoMdPhonePortrait } from "react-icons/io";
 
 function App() {
 
   return (
     <>
+
       <div className="Splash-Page Hero flex flex-row px-10 h-screen items-center">
         <div className="Hero-Left">
           <h3 className="text-2xl text-accent font-bold">Hi, I'm</h3>
@@ -28,31 +32,31 @@ function App() {
         <hr className="w-48 border-2 border-accent rounded-full mt-2.5" />
         <div className="Splash-Page-Cards flex flex-row gap-20 mt-15">
           <div className="Splash-Page-Card flex flex-col items-center">
-            <FaLaptopCode className="text-primary bg-card text-8xl rounded-full p-10" />
+            <MdOutlineComputer className="text-accent bg-card text-8xl rounded-full p-5" />
             <p className="text-primary">Web Development</p>
             <p className="text-secondary">Description</p>
           </div>
 
           <div className="Splash-Page-Card flex flex-col items-center">
-            <FaLaptopCode className="text-primary bg-card text-8xl rounded-full p-10" />
+            <IoMdPhonePortrait className="text-accent bg-card text-8xl rounded-full p-5" />
             <p className="text-primary">App Development</p>
             <p className="text-secondary">Description</p>
           </div>
 
           <div className="Splash-Page-Card flex flex-col items-center">
-            <FaLaptopCode className="text-primary bg-card text-8xl rounded-full p-10" />
+            <FaChartPie className="text-accent bg-card text-8xl rounded-full p-5" />
             <p className="text-primary">Problem Solving</p>
             <p className="text-secondary">Description</p>
           </div>
 
           <div className="Splash-Page-Card flex flex-col items-center">
-            <FaLaptopCode className="text-primary bg-card text-8xl rounded-full p-10" />
+            <IoCamera className="text-accent bg-card text-8xl rounded-full p-5" />
             <p className="text-primary">Content Creation</p>
             <p className="text-secondary">Description</p>
           </div>
 
           <div className="Splash-Page-Card flex flex-col items-center">
-            <FaLaptopCode className="text-primary bg-card text-8xl rounded-full p-10" />
+            <TbChefHatFilled className="text-accent bg-card text-8xl rounded-full p-5" />
             <p className="text-primary">Cooking</p>
             <p className="text-secondary">Description</p>
           </div>
