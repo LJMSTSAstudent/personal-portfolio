2bdec6b155816000897f0298f75dc095e0cd7a21
Vaibhav <vaibhav.saravanan@gmail.com>
2026-09-20 14:24:31 -0400

    style: remove text from linked icons

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -7,27 +7,22 @@ function Contact() {
     return (
     <div>
         <PageHeader />
-        <div className="content flex flex-row items-center py-2.5">    
+        <div className="content flex flex-row items-center py-2.5 pl-10">    
             <div className="Left content-center px-10 my-5 h-[71vh]">
                 <h3 className="text-6xl text-primary font-bold">Let's Connect</h3>
                 <hr className="w-20 border-2 rounded-xl border-accent mt-1.5 mb-3" />
                 <h3 className="text-xl text-secondary">I'm always open to new opportunities, collaborations, or a friendly hello!</h3>
             </div>
             <div className="Right">
-                <div className="Footer-Right flex flex-row gap-10">
-                    <div className="Icon-Card flex flex-row gap-3">
-                        <a href="mailto: vaibhav.saravanan@gmail.com"><FaEnvelope className="text-accent text-6xl" /></a>
-                        <div className="Text">
-                            <h3 className="text-xl text-primary">Email</h3>
-                            <h3 className="text-base text-secondary">Vaibhav.saravanan@gmail.com</h3>
-                        </div>
+                <div className="Footer-Right flex flex-row gap-20 pl-30">
+                    <div className="Icon-Card">
+                        <a href="mailto: vaibhav.saravanan@gmail.com"><FaEnvelope className="text-accent text-8xl" /></a>
                     </div>
-                    <div className="Icon-Card flex flex-row gap-3">
-                        <a href="https://www.linkedin.com/in/ksaravanan"><FaLinkedin className="text-accent text-6xl" /></a>
-                        <div className="Text">
-                            <h3 className="text-xl text-primary">Linkedin</h3>
-                            <h3 className="text-base text-secondary">Vaibhav Saravanan</h3>
-                        </div>
+                    <div className="Icon-Card">
+                        <a href="https://www.linkedin.com/in/ksaravanan"><FaLinkedin className="text-accent text-8xl" /></a>
+                    </div>
+                    <div className="Icon-Card">
+                        <a href="https://github.com/LJMSTSAstudent"><FaGithub className="text-accent text-8xl" /></a>
                     </div>
                 </div>
             </div>
